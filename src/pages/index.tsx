41a401ed91a81642d57a5a6a131320c2d2a1869a
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-15 12:30:47 +0100

    render Field

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,9 @@ export default function Home() {
     <Card>
       <CardContent>
         <Formik>
-          <Form></Form>
+          <Form>
+            <Field></Field>
+          </Form>
         </Formik>
       </CardContent>
     </Card>
