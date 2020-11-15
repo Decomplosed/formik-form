7c85e606b2d3dc04d5a7d72f15f9a442c96d2285
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-15 12:32:34 +0100

    imported in CheckboxWithLabel, TextField

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, Typography } from '@material-ui/core';
 import { Field, Form, Formik } from 'formik'
+import { CheckboxWithLabel, TextField } from 'formik-material-ui'
 
 export default function Home() {
   return (
