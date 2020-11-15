c79b392279dbe8f6eda4616faa9e453c6036ebf3
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-15 13:00:40 +0100

    wrapped steps inside div container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,11 +24,17 @@ export default function Home() {
           }} onSubmit={() => {}}
         >
           <Form autoComplete='off'>
-            <Field name='firstName' component={TextField} label='First Name' />
-            <Field name='lastName' component={TextField} label='Last Name' />
-            <Field name='millionaire' type='checkbox' component={CheckboxWithLabel} Label={{label: 'I am a millionaire'}} />
-            <Field name='money' type='number' component={TextField} label='All the money I have' />
-            <Field name='description' component={TextField} label='Description' />
+            <div>
+              <Field name='firstName' component={TextField} label='First Name' />
+              <Field name='lastName' component={TextField} label='Last Name' />
+              <Field name='millionaire' type='checkbox' component={CheckboxWithLabel} Label={{label: 'I am a millionaire'}} />
+            </div>
+            <div>
+              <Field name='money' type='number' component={TextField} label='All the money I have' />
+            </div>
+            <div>
+              <Field name='description' component={TextField} label='Description' />
+            </div>
           </Form>
         </Formik>
       </CardContent>
