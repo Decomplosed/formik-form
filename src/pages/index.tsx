584eda16366a1c3588dc6e8e407fa51693148ba2
Bart Frackiewicz <decomplosed@gmail.com>
2020-11-15 12:45:16 +0100

    changed label for money input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ export default function Home() {
             <Field name='firstName' component={TextField} label='First Name' />
             <Field name='lastName' component={TextField} label='Last Name' />
             <Field name='millionaire' type='checkbox' component={CheckboxWithLabel} Label={{label: 'I am a millionaire'}} />
-            <Field name='money' type='number' component={TextField} label='Money' />
+            <Field name='money' type='number' component={TextField} label='All the money I have' />
             <Field name='description' component={TextField} label='Description' />
           </Form>
         </Formik>
